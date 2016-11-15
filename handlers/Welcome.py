61d20f34cba45a430690c2f1d55c9045b9dd8caa
Kristine <kristine@BrisMac2013.local>
2016-11-15 11:31:11 -0500

    corrected error with module link

--- handlers/Welcome.py
+++ handlers/Welcome.py
@@ -1,4 +1,4 @@
-from fx import functions
+
 from handlers import BlogHandler
 from models import BlogEntry, Comments, Likes, Users
 from functions import hash_str, make_secure_val, check_secure_val, make_bcrypt_hash, validate_bcrypt, make_cookie_hash, render_str, set_secure_cookie, login, auth
